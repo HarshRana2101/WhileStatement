a34a02a9d6e3f1af7e7426973a46e3cde5bcb583
harsh <harshrana2101@gmail.com>
2020-06-05 13:24:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int count = 1;
+        while (count != 6) {
+            System.out.println("count value is " + count);
+            count++;
+        }
+
+
+    }
+}
